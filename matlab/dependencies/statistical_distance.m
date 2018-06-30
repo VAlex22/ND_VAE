7b943869a436bcb49f4dd3a1a962cc13a05cfc3b
VAlex22 <vasilyev2208@gmail.com>
2018-06-30 19:01:18 +0200

    Update statistical_distance.m

--- matlab/dependencies/statistical_distance.m
+++ matlab/dependencies/statistical_distance.m
@@ -1,8 +1,10 @@
 function D = statistical_distance(A, B)
-% Compute square Euclidean or Mahalanobis distances between all pair of vectors.
+% Compute square Bhattacharyya distances between all pair of vectors.
 %   A: d x n1 x 2 data matrix of d-dimensional normal distributions
 %   B: d x n2 x 2 data matrix of d-dimensional normal distributions
 %   D: n1 x n2 pairwise Bhattacharyya distance
+% Implementation is based on the Michael Chen implementation of Euclidean 
+% distance matrix computation (see sqdistance.m)
 
 assert(size(A,1)==size(B,1));
 assert(size(A,3)==size(B,3));
