bb6640fa2cd6558f9ceca185a9735b90aac433f5
VAlex22 <vasilyev2208@gmail.com>
2018-06-30 18:59:45 +0200

    Update sqdistance.m

--- matlab/dependencies/sqdistance.m
+++ matlab/dependencies/sqdistance.m
@@ -25,10 +25,10 @@
 
 function D = sqdistance(A, B, M)
 % Compute square Euclidean or Mahalanobis distances between all pair of vectors.
-   A: d x n1 data matrix
-   B: d x n2 data matrix
-   M: d x d  Mahalanobis matrix
-   D: n1 x n2 pairwise square distance matrix
+%   A: d x n1 data matrix
+%   B: d x n2 data matrix
+%   M: d x d  Mahalanobis matrix
+%   D: n1 x n2 pairwise square distance matrix
 % Written by Michael Chen (sth4nth@gmail.com).
 if nargin == 1
     A = bsxfun(@minus,A,mean(A,2));
