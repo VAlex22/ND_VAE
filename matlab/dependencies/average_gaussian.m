38fecb46e51bbbe7a365e9a70eaa8dad629c7ef5
VAlex22 <vasilyev2208@gmail.com>
2018-08-08 21:47:57 +0200

    Update average_gaussian.m

--- matlab/dependencies/average_gaussian.m
+++ matlab/dependencies/average_gaussian.m
@@ -4,8 +4,6 @@ assert(size(A,1)==size(B,1));
 assert(size(A,3)==size(B,3));
 
 mu = bsxfun(@minus, A(:,:,1)', reshape(B(:,:,1), 1, size(B, 1), []));
-%sig = permute(repmat(A(:,:,2), [1, 1, size(B, 2)]), [2, 1, 3]);
 sig = permute(A(:,:,2), [2, 1]);
 
-%D = squeeze(exp(dot(mu./(sig.^2), mu, 2)))./(squeeze(prod(sig, 2)*sqrt(2*pi)));
 D = squeeze(exp(-0.5*dot(mu./(sig.^2), mu, 2)))./(squeeze(prod(sig, 2)*sqrt(2*pi)));
